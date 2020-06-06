2ea785450d85d81a72f69eb4632aa2c300b37814
Joaquin Nader <joaquinnader@gmail.com>
2020-06-06 02:55:13 -0300

    Improved readme and created tasks for the project execution

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
-## Endpoints
-https://www.reddit.com/top.json?limit=10&t=month&after=<Provided after first request>
+## How To Run
+```sh
+$ cd react-redux-ts
+$ yarn install
+$ yarn start
+```
+
+## Required
+- Node >= v10.16.3
+- Yarn >= v1.22.4
+
+## Environment Variables
+Since this project is not using predefiend configuration for eslint therefore it's needed to overrid that configuration following the `.env.example` file.
+
+## Tasks
+[x] Setup Project
+[ ] Decide Folder structure
+[ ] Create Responsive Main Layout
+[ ] Create main logic and retrieve all posts from reddit + UT
+[ ] Create Post Payload to filter Post information + UT
+[ ] Create Post List component - Drawer + Detail
+[ ] Show post detail on click + UT - Expandable vs Modal
+[ ] Remove Post from list + UT
+[ ] Remove all posts + UT
+[ ] Fetch more posts + UT
+[ ] Pagination vs Infinite Scrolling + UT
+[ ] Filter + UT
+[ ] Add CSS Animations
 
 ## Online Resources
-https://www.npmjs.com/package/cra-template-redux-typescript
-https://github.com/typescript-eslint/typescript-eslint/issues/1845
-https://github.com/reduxjs/redux-toolkit/blob/master/docs/api/createSlice.md
+- https://www.npmjs.com/package/cra-template-redux-typescript
+- https://enbonnet.me/article/46/usar-eslint-y-prettier-en-proyectos-typescript
+- https://github.com/typescript-eslint/typescript-eslint/issues/1845
+- https://github.com/reduxjs/redux-toolkit/blob/master/docs/api/createSlice.md
+
+## Endpoints
+https://www.reddit.com/top.json?limit=10&t=month&after=<Provided after first request>
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -14,16 +14,11 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1;
     },
     decrement: (state) => {
       state.value -= 1;
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     }
@@ -32,10 +27,7 @@ export const counterSlice = createSlice({
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
+/* Action Creators, used to call async methods */
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const incrementAsync = (amount: number): AppThunk => (dispatch) => {
   setTimeout(() => {
@@ -43,9 +35,7 @@ export const incrementAsync = (amount: number): AppThunk => (dispatch) => {
   }, 1000);
 };
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+/* Selectors */
 export const selectCount = (state: RootState): number => state.counter.value;
 
 export default counterSlice.reducer;
